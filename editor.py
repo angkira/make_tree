10937bf5f87514c913df0aeaa0fdefba1551e6b1
angkira <angkira@yandex.ru>
2016-02-12 22:12:32 +0300

    v.0.1.8

--- editor.py
+++ editor.py
@@ -5,7 +5,6 @@ with open("source.json", 'r') as out: data = out.read()
 def run_editor(ARGS, path):     #if there is "-r"-flag => run FILES
 	EDITOR, files = json.loads(data)['EDITOR'], json.loads(data)['FILES'].keys()
 	for arg in ARGS:
-	    for var in VAR_PATH:
-	        if arg in sys.path: EDITOR = [arg,]#flag set new EDITOR "arg"
+		if arg in sys.path: EDITOR = [arg,]#flag set new EDITOR "arg"
 	print (path)
 	call(EDITOR+[path+file for file in files])
