8efb666d37665153d66fbc39e7dbc00bfcf1a2c4
Yuriy <angkira@yandex.ru>
2016-02-14 14:41:59 +0300

    v.0.2.3
    - Some fixes

--- apps.py
+++ apps.py
@@ -9,6 +9,5 @@ def run_editor(ARGS, path):     #if there is "-r"-flag => run FILES
 	print (path)
 	call(EDITOR+[path+file for file in files], shell=True)
 def open_browser(path):
-	BROWSER =  json.loads(data)['BROWSER']
-	if sys.platform == 'win32': BROWSER = 'start '
+	BROWSER = 'start ' if sys.platform == 'win32' else json.loads(data)['BROWSER']
 	call(BROWSER+' '+path+'index.html', shell = True)
