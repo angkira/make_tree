2d3df0492677252dcbb3a78452d1dc922041a400
Yuriy <angkira@yandex.ru>
2016-02-22 03:44:18 +0300

    v0.2.5.2

--- apps.py
+++ apps.py
@@ -1,4 +1,4 @@
-import sys
+import os
 from subprocess import call
 import json
 with open("source.json", 'r') as out: data = out.read()
@@ -10,10 +10,11 @@ def help():
 def run_editor(ARGS, path):
 	EDITOR, files = json.loads(data)['EDITOR'], json.loads(data)['FILES'].keys()
 	for arg in ARGS[1:]:
-		for folder in sys.path:
-			if arg in folder: EDITOR = [arg,]
+		for folder in os.environ['PATH'].split(os.pathsep):
+			if os.path.isdir(folder) and arg in os.listdir(folder): EDITOR = [folder+'/'+arg,]
 	call(EDITOR+[path+file_ for file_ in files], shell=True)
 
 def open_browser(path):
-	BROWSER = 'start ' if sys.platform == 'win32' else json.loads(data)['BROWSER']
+	BROWSER = 'start ' if os.name == 'nt' else json.loads(data)['BROWSER']
 	call(BROWSER+' '+path+'index.html', shell = True)
+run_editor('brackets', os.getcwd())
