f87b32add170db557fb9653525c33ba905762d16
angkira <angkira@yandex.ru>
2016-03-21 15:04:51 +0300

    v.0.3.1

--- make_tree.py
+++ make_tree.py
@@ -4,7 +4,7 @@ from apps import *
 def read_flags():
     FLAGS = json.loads(data)['FLAGS']
     for arg in ARGS:
-        if (arg in FLAGS.keys()) and (FLAGS[arg] == 'help'):
+        if !(arg in FLAGS.keys()) and (FLAGS[arg] == 'help'):
             help()
             break
         else:
